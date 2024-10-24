df7d9c052dcfcd494c0a59b863f1a3881abb7ffb
Marcelo Peixoto <130364951+marcelopxt@users.noreply.github.com>
2024-10-24 09:18:24 -0300

    Exercicio 5 com tratamentos de erros

--- Recursividade/src/Exercicio5.java
+++ Recursividade/src/Exercicio5.java
@@ -1,18 +1,22 @@
 import java.util.Scanner;
 
 public class Exercicio5 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite dois numeros para saber o maximo divisor comum!");
-        System.out.println("1° numero: ");
-        int a = sc.nextInt();
-        System.out.println("2° numero: ");
-        int b = sc.nextInt();
-        sc.close();
-        int result = calcularMdc(a, b);
-        System.out.println("O maximo divisor comum é: " + result);
+        try {
+            System.out.println("Digite dois numeros para saber o maximo divisor comum!");
+            System.out.println("1° numero: ");
+            int a = sc.nextInt();
+            System.out.println("2° numero: ");
+            int b = sc.nextInt();
+            sc.close();
+            int result = calcularMdc(a, b);
+            System.out.println("O maximo divisor comum é: " + result);
+        } catch (Exception e) {
+           System.out.println("Digite um numero válido.");
+        }
     }
-    public static int calcularMdc(int a, int b) {
+    public static int calcularMdc(int a, int b) throws Exception {
         if(a >= b && a % b == 0){
             return  b;
         }else if(a < b){
