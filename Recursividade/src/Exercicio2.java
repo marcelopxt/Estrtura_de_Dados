a4732cdca93b99f39a14bbbf495b054a386b267d
Marcelo Peixoto <130364951+marcelopxt@users.noreply.github.com>
2024-10-24 09:16:43 -0300

    Exercicio 2 com Tratamento de erros

--- Recursividade/src/Exercicio2.java
+++ Recursividade/src/Exercicio2.java
@@ -1,17 +1,21 @@
 import java.util.Scanner;
 
 public class Exercicio2 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        try {
+            System.out.print("Informe um número para usar como potencia de 2: ");
+            int n = sc.nextInt();
+            sc.close();
+            System.out.println("o resultado de  2^" + n + " é: " + calcularRecursiva(n));
 
-        System.out.print("Informe um número para usar como potencia de 2: ");
-        int n = sc.nextInt();
-        sc.close();
-
-        System.out.println("o resultado de  2^" + n + " é: " + calcularRecursiva(n));
+        } catch (Exception e) {
+           System.out.println("Digite um número válido.");
+        }
+        
     }
 
-    public static double calcularRecursiva(double n) {
+    public static double calcularRecursiva(double n) throws Exception {
         if (n < 0) {
             return 1.0/(2 * calcularRecursiva(-n - 1));
         }else if (n == 0) {
