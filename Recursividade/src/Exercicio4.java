6693c19a9c96cb407a6d517904dc27a05d4df5b2
Marcelo Peixoto <130364951+marcelopxt@users.noreply.github.com>
2024-10-24 09:17:56 -0300

    Exercicio 4 com Tratamento de erros

--- Recursividade/src/Exercicio4.java
+++ Recursividade/src/Exercicio4.java
@@ -1,22 +1,26 @@
 import java.util.Scanner;
 
 public class Exercicio4 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite um número usar como base da multiplicação: ");
-        int base = sc.nextInt();
-        System.out.println("Digite um número para ser multiplicado pela base: ");
-        int num = sc.nextInt();
-        sc.close();
-        int result = multiplicarRecursiva(base, num);
-        System.out.println("O resultado dessa multiplicação é: " + result);
+        try {
+            System.out.println("Digite um número usar como base da multiplicação: ");
+            int base = sc.nextInt();
+            System.out.println("Digite um número para ser multiplicado pela base: ");
+            int num = sc.nextInt();
+            sc.close();
+            int result = multiplicarRecursiva(base, num);
+            System.out.println("O resultado dessa multiplicação é: " + result);
+        } catch (Exception e) {
+           System.out.println("Digite um numero válido.");
+        }
     }
-    public static int multiplicarRecursiva(int xbase, int xnum){
+    public static int multiplicarRecursiva(int xbase, int xnum) throws Exception{
         int total = 0;
         return multiplicarRecursiva(xbase, xnum, total);
     }
 
-    public static int multiplicarRecursiva(int xbase, int xnum, int total){
+    public static int multiplicarRecursiva(int xbase, int xnum, int total) throws Exception{
         if(xbase == 0){
             return total;
         }
