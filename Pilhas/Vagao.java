951d04f70afdbef86ca581d1b6d9c0af3a3810bc
Marcelo Peixoto <130364951+marcelopxt@users.noreply.github.com>
2024-12-06 19:16:10 -0300

    Create Vagao.java

---
+++ Pilhas/Vagao.java
@@ -0,0 +1,16 @@
+public class Vagao {
+    private int num;
+
+    public Vagao(int num){
+        this.num = num;
+    }
+    public Vagao(){
+
+    }
+    public int getNum() {
+        return this.num;
+    }
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
