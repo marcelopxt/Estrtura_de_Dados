e6b9cb4c571456950500430bb21f84916db46dc8
Marcelo Peixoto <130364951+marcelopxt@users.noreply.github.com>
2025-01-24 16:51:48 -0300

    Create Pessoa.java

---
+++ FIla/FilaReferencia/FilaNomes/Pessoa.java
@@ -0,0 +1,13 @@
+package SemGenerics;
+
+public class Pessoa{
+    String nome;
+    String ope;
+    
+    public Pessoa(String nome, String ope) {
+        this.nome = nome;
+        this.ope = ope;
+    }
+
+    
+}
