3b3019adfa866ceeeec9e4cc2b7627b7485b84a6
Marcelo Peixoto <130364951+marcelopxt@users.noreply.github.com>
2025-03-05 20:14:30 -0300

    Add files via upload

---
+++ Arvore/Exercicio1/Child.java
@@ -0,0 +1,14 @@
+public class Child {
+    int height;
+
+    public Child(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Child{" +
+                "height=" + height +
+                '}';
+    }
+}
